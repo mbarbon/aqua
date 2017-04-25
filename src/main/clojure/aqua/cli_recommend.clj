5bbada4f7684142618a6c086c2e345d16ad02202
Mattia Barbon <mattia@barbon.org>
2017-04-25 23:08:54 +0200

    Don't hardcode the user name

--- src/main/clojure/aqua/cli_recommend.clj
+++ src/main/clojure/aqua/cli_recommend.clj
@@ -12,9 +12,10 @@
         [users recommended] (known-anime-tagger
                               (aqua.recommend.cosine/get-recommendations user users known-anime-filter))
         [_ recommended-airing] (known-anime-tagger [users (aqua.recommend.collaborative-filter/recommended-airing users airing-anime-filter)])]
+    (println "User" (.username user) (count (seq (.completedAndDropped user))))
     (println "Users")
     (doseq [[score user] users]
-      (println (.username user) score))
+      (println (.username user) (count (seq (.completedAndDropped user))) score))
     (println)
     (println "Airing anime")
     (doseq [[anime-id score tag] (take 15 recommended-airing)]
@@ -26,10 +27,9 @@
       (let [anime (anime-map anime-id)]
         (println tag anime-id (.title anime) score)))))
 
-(defn -main []
+(defn -main [username]
   (println "Starting")
   (let [directory "maldump"
-        username "mattia_y"
         data-source (aqua.mal-local/open-sqlite-ro directory "maldump.sqlite")
         _ (println "Loading users")
         user (aqua.mal-local/load-user data-source username)
