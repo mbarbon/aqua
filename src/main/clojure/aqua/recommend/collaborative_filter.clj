3c440662a4777d566ccc4b947232fd8604571a89
Mattia Barbon <mattia@barbon.org>
2017-04-27 15:14:04 +0200

    Use a Java HashMap as the intermediate data structure rather than a Clojure map
    It gains some speed because of mutability, and some because it uses a trivial
    hash function for integers, as opposed to Clojure using Murmur.

--- src/main/clojure/aqua/recommend/collaborative_filter.clj
+++ src/main/clojure/aqua/recommend/collaborative_filter.clj
@@ -14,29 +14,43 @@
       (< completed-max (* 2 completed-min)) true
       :else false)))
 
+(defn- mutating-merge-with [fun & rest]
+  (let [result (java.util.HashMap.)]
+    (doseq [merge-item rest]
+      (doseq [^java.util.Map$Entry entry merge-item]
+        (if-let [current-value (.get result (.getKey entry))]
+          (.put result (.getKey entry) (fun current-value (.getValue entry)))
+          (.put result (.getKey entry) (.getValue entry)))))
+    result))
+
 (defn- score-completed-anime [remove-known-anime user user-rank]
-  (into {}
-    (for [^aqua.mal.data.Rated item (remove-known-anime (.completed user))]
+  (let [result (java.util.HashMap.)]
+    (doseq [^aqua.mal.data.Rated item (remove-known-anime (.completed user))]
       ; [(.animedbId item) (- (* (.normalizedRating item) (- user-rank)))])))
-      [(.animedbId item) (if (>= (.normalizedRating item) 0)
-                           (- (.normalizedRating item))
-                           0.5)])))
+      (.put result (.animedbId item) (if (>= (.normalizedRating item) 0)
+                                       (- (.normalizedRating item))
+                                       0.5)))
+    result))
+
+(defn- hash-entry-to-pair [^java.util.Map$Entry entry]
+  [(.getKey entry) (.getValue entry)])
 
 (defn recommended-completed [similar-users remove-known-anime]
-  (let [scored-anime (apply merge-with min
+  (let [scored-anime (apply mutating-merge-with min
                        (for [[rank similar-user] similar-users]
                          (score-completed-anime remove-known-anime similar-user rank)))
-        ranked-anime (sort-by #(% 1) (seq scored-anime))]
+        ranked-anime (sort-by #(% 1) (map hash-entry-to-pair (seq scored-anime)))]
     ranked-anime))
 
 (defn- score-airing-anime [keep-airing-anime user user-rank]
-  (into {}
-    (for [^aqua.mal.data.Rated item (keep-airing-anime (.watching user))]
-      [(.animedbId item) user-rank])))
+  (let [result (java.util.HashMap.)]
+    (doseq [^aqua.mal.data.Rated item (keep-airing-anime (.watching user))]
+      (.put result (.animedbId item) user-rank))
+    result))
 
 (defn recommended-airing [similar-users keep-airing-anime]
-  (let [scored-anime (apply merge-with +
+  (let [scored-anime (apply mutating-merge-with +
                        (for [[rank similar-user] similar-users]
                          (score-airing-anime keep-airing-anime similar-user rank)))
-        ranked-anime (sort-by #(% 1) (seq scored-anime))]
+        ranked-anime (sort-by #(% 1) (map hash-entry-to-pair (seq scored-anime)))]
     ranked-anime))
