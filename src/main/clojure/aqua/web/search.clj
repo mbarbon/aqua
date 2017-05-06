46d02bdfa2a078ec335f3ce088beff32a1ab6e97
Mattia Barbon <mattia@barbon.org>
2017-05-06 22:02:19 +0200

    More logging

--- src/main/clojure/aqua/web/search.clj
+++ src/main/clojure/aqua/web/search.clj
@@ -1,13 +1,16 @@
 (ns aqua.web.search
   (:require aqua.search.autocomplete
             aqua.web.render
+            [clojure.tools.logging :as log]
             [aqua.web.globals :refer [*data-source-ro *suggest *anime]]))
 
 (defn- rebuild-suggester []
+  (log/info "Start loading suggester")
   (let [data-source @*data-source-ro
         anime-titles (aqua.mal-local/load-anime-titles data-source)
         anime-rank (aqua.mal-local/load-anime-rank data-source)
         suggest (aqua.search.autocomplete/prepare-suggest anime-titles anime-rank)]
+    (log/info "Done loading suggester")
     (reset! *suggest suggest)))
 
 (defn init []
