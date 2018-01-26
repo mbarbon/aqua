81a2f6f2acf72472ca27bcba9ffbc2d42ce73bfd
Mattia Barbon <mattia@barbon.org>
2018-01-26 21:14:22 +0100

    Import aqua.css in the application, to benefit from the build pipeline
    The CSS file is still available separately for the privacy page.

--- src/main/js/web/index.js
+++ src/main/js/web/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import '../../../../src/main/resources/public/css/aqua.css'
 
 ReactDOM.render(<App />, window.document.getElementById('root'))
 registerServiceWorker()
