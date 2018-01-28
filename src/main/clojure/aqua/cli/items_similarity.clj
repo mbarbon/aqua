ca61aee961a492da3a5300305b1cb44dd670085e
Mattia Barbon <mattia@barbon.org>
2018-01-28 12:32:23 +0100

    Add co-occurrency model to the item similarity check script

--- src/main/clojure/aqua/cli/items_similarity.clj
+++ src/main/clojure/aqua/cli/items_similarity.clj
@@ -1,7 +1,14 @@
 (ns aqua.cli.items-similarity
   (:require aqua.mal-local
             aqua.recommend.lfd-items
+            aqua.recommend.co-occurrency
             aqua.recommend.rp-similar-anime))
+
+(defn- print-similar [model-name anime scored-anime]
+  (.sort scored-anime aqua.recommend.ScoredAnimeId/SORT_SCORE)
+  (println (str "\n" model-name))
+  (doseq [scored scored-anime]
+    (println (.animedbId scored) (.title (anime (.animedbId scored))) (.score scored))))
 
 (defn -main [animedb-string]
   (println "Starting")
@@ -10,17 +17,14 @@
         animedb-id (Integer/valueOf animedb-string)
         _ (println "Loading models")
         lfd-items (aqua.recommend.lfd-items/load-lfd-items "maldump/lfd-items-model" "maldump/lfd-items-model-airing")
+        co-occurrency (aqua.recommend.co-occurrency/load-co-occurrency "maldump/co-occurrency-model" "maldump/co-occurrency-model-airing")
         rp-model (aqua.recommend.rp-similar-anime/load-rp-similarity "maldump/rp-model")
         _ (println "Loading anime")
         anime (aqua.mal-local/load-anime data-source)
         scored-anime-lfd (.similarAnime (.complete lfd-items) animedb-id)
+        scored-anime-co-occurrency (.similarAnime (.complete co-occurrency) animedb-id)
         scored-anime-rp (.similarAnime rp-model animedb-id)]
-    (.sort scored-anime-lfd aqua.recommend.ScoredAnimeId/SORT_SCORE)
-    (.sort scored-anime-rp aqua.recommend.ScoredAnimeId/SORT_SCORE)
     (println "Anime similar to " (.title (anime animedb-id)))
-    (println "\nLFD")
-    (doseq [scored scored-anime-lfd]
-      (println (.animedbId scored) (.title (anime (.animedbId scored))) (.score scored)))
-    (println "\nRP")
-    (doseq [scored scored-anime-rp]
-      (println (.animedbId scored) (.title (anime (.animedbId scored))) (.score scored)))))
+    (print-similar "LFD" anime scored-anime-lfd)
+    (print-similar "Co-occurrency" anime scored-anime-co-occurrency)
+    (print-similar "RP" anime scored-anime-rp)))
