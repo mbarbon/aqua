6b8c5e96ec0b7608cce8117c502b69dde3f273a4
Mattia Barbon <mattia@barbon.org>
2018-02-02 21:51:58 +0100

    Tweak co-occurrency model parameters
    It is still biased towards anime that aired in the same season, but there isn't
    much that can be done about it without drastically changing the scoring
    algorithm, and even then the results might not be much better.

--- src/main/clojure/aqua/cli/recompute_models.clj
+++ src/main/clojure/aqua/cli/recompute_models.clj
@@ -18,8 +18,8 @@
 
 (def user-sample-count 100000)
 
-(def co-occurrency-score-threshold 0.2)
-(def co-occurrency-alpha 0.3)
+(def co-occurrency-score-threshold 0.3)
+(def co-occurrency-alpha 0.6)
 (def co-occurrency-item-count 30)
 (def co-occurrency-item-count-airing 15)
 
