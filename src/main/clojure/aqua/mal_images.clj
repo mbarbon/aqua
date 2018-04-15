897cd87449b965e8bc884f19ffbd480fb55bf742
Mattia Barbon <mattia@barbon.org>
2018-04-15 15:39:10 +0200

    Make sure the result is an integral type, otherwise we can't store it

--- src/main/clojure/aqua/mal_images.clj
+++ src/main/clojure/aqua/mal_images.clj
@@ -42,7 +42,7 @@
         expires (if-let [expires-header (:expires image-data)]
                   (aqua.mal.Http/parseDate expires-header)
                   ; give it another day
-                  (+ 86400 (/ 1000 (java.lang.System/currentTimeMillis))))]
+                  (+ 86400 (long (/ 1000 (java.lang.System/currentTimeMillis)))))]
     (when (and changed path)
       (write-file (io/file directory path) (io/file directory tmp-path) (:image image-data))
       (let [image (javax.imageio.ImageIO/read (java.io.ByteArrayInputStream. (:image image-data)))
